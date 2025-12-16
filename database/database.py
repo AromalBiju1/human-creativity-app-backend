c7cad43f46167887d30052e3945b93ce813698fd
Aromal Biju <aromalbiju363@gmail.com>
2025-12-16 12:40:28 +0530

    Handled backend for login and signup

--- database/database.py
+++ database/database.py
@@ -1,11 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-from dotenv import load_dotenv
+from config.config import database_url
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 Base = declarative_base()
 
